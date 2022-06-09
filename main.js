d671d95f7fa9e0994dc495ab31e29f06402fee10
MargothAmz <93557612+MargothAmz@users.noreply.github.com>
2022-06-09 02:02:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/*Recuerda toda instruccion termina con...?,
+encuentra el error*/
+var playerPaddle, computerPaddle, ball,
+
+function setup(){
+    // Agrega el código para crear los sprites
+ 
+  
+  
+}
+
+function draw(){
+  background("black");
+   // Llama a la función draw debajo
+}
